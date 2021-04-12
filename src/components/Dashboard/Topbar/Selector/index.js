16acae4d0f552a1a7d948f4496450ff68c41c99f
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-04-12 15:31:49 +0200

    Updated the group/label selector

--- src/components/Dashboard/Topbar/Selector/index.js
+++ src/components/Dashboard/Topbar/Selector/index.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { ChevronDown } from 'react-feather';
 import { useLazyQuery } from '@apollo/client';
 
-import ModalContext from '../../../../context/modal-context';
 import DatabaseContext from '../../../../context/database-context';
 
 import { LABEL_EVENTS } from '../../../../graphql/events';
@@ -9,54 +9,42 @@ import { LABEL_EVENTS } from '../../../../graphql/events';
 import './index.scss';
 
 const Selector = () => {
+  const [visible, setVisible] = useState(true);
+
   const [label, setLabel] = useState('Choisir un groupe.');
   const [labelId, setLabelId] = useState('');
 
-  const { hideModal, showModal, setModalTitle, setModalContent } = useContext(ModalContext);
   const { setEvents, labels } = useContext(DatabaseContext);
 
-  const [getEvents, { error, data }] = useLazyQuery(LABEL_EVENTS, { variables: { label_id: labelId } });
+  const [getEvents, { data }] = useLazyQuery(LABEL_EVENTS, { variables: { label_id: labelId } });
 
-  useEffect(() => {
-    if (data) {
-      hideModal();
-      setEvents(data.labelEvents);
+  useEffect(() => data && setEvents(data.labelEvents), [data]);
 
-      // Handle data.
-    }
-  }, [data]);
+  const switchVisibility = () => setVisible((prev) => !prev);
 
-  useEffect(() => {
-    error && console.log(error);
-  }, [error]);
-
-  const handleChangeScheduleLabel = ({ label_name, id }) => {
+  const handleLabelClick = (_event, { id, label_name }) => {
+    setVisible(false);
     setLabelId(id);
+
     setLabel(label_name);
+
     getEvents();
   };
 
-  const handleClick = () => {
-    setModalTitle('Choisir un groupe.');
-
-    setModalContent(
-      <div className='container'>
-        <div className='table'>
-          {labels.map((label) => (
-            <div key={label.id}>
-              <p onClick={() => handleChangeScheduleLabel(label)}>{label.label_name}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-
-    showModal();
-  };
-
   return (
-    <div onClick={handleClick} className='label-picker'>
-      <p>{label}</p>
+    <div className='label-picker'>
+      <div className={`label-header ${visible ? 'active' : ''}`} onClick={switchVisibility}>
+        <p>{label}</p>
+        <ChevronDown />
+      </div>
+
+      <ul className={`label-selector ${visible ? 'visible' : 'hidden'}`}>
+        {labels.map(({ id, label_name }) => (
+          <li className='item' key={id} onClick={(e) => handleLabelClick(e, { id, label_name })}>
+            {label_name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
