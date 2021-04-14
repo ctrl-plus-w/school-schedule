3e4cb1b15f79839103cb6317e21354437c9b4fd3
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-04-14 12:21:34 +0200

    Remove testing data from useError

--- src/hooks/useError.js
+++ src/hooks/useError.js
@@ -1,14 +1,7 @@
 import { useState } from 'react';
 
-import { v4 } from 'uuid';
-
 const useError = () => {
-  const [errors, setErrors] = useState({
-    [v4()]: {
-      title: 'Error',
-      message: 'Something wrong happend.',
-    },
-  });
+  const [errors, setErrors] = useState({});
 
   // eslint-disable-next-line no-unused-vars
   const removeKey = (key, { [key]: _, ...rest }) => rest;
