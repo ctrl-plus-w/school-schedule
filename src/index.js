e2850edd16994061e58f81e482128938600364e5
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-04-15 16:38:33 +0200

    Setup of redux and creation of the store.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Provider } from 'react-redux';
+
 import './styles/_global.scss';
 
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 
+import store from './app/store';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
