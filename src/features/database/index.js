0eeb581ce54f9d40da690716f41bdfdcff976a64
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-04-16 14:34:12 +0200

    Added the labels reducer.

--- src/features/database/index.js
+++ src/features/database/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import authReducer from './authSlice';
 import eventsReducer from './eventsSlice';
+import labelsReducer from './labelsSlice';
 
 export default combineReducers({
   events: eventsReducer,
   auth: authReducer,
+  labels: labelsReducer,
 });
