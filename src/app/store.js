f8931c168504c7a1553faff3b2db96da20f49396
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-04-16 14:29:47 +0200

    Added the logger and set two reducers.

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
 
 import databaseReducer from '../features/database';
+import modalsReducer from '../features/modals';
 
 const store = configureStore({
-  reducer: databaseReducer,
-  middleware: (getDefaultMiddleware) => [...getDefaultMiddleware()],
+  reducer: {
+    database: databaseReducer,
+    modals: modalsReducer,
+  },
+
+  middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), logger],
+
   devTools: true,
 });
 
