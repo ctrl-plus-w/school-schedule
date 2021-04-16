1c2919a4b86e5963b9083fd75c85ddac0fb0afcd
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-04-16 17:06:37 +0200

    Added the addEvent and removeEvent reducers;

--- src/features/infos/infosSlice.js
+++ src/features/infos/infosSlice.js
@@ -5,6 +5,8 @@ const slice = createSlice({
 
   initialState: {
     selectedLabel: '',
+
+    selectedEvents: {},
   },
 
   reducers: {
@@ -12,11 +14,23 @@ const slice = createSlice({
       ...state,
       selectedLabel: action.payload,
     }),
+
+    addEvent: (state, action) => {
+      const day = state.selectedEvents[action.payload.date];
+      if (!day) state.selectedEvents[action.payload.date] = [action.payload.start];
+      else state.selectedEvents[action.payload.date].push(action.payload.start);
+    },
+
+    removeEvent: (state, action) => {
+      const d = state.selectedEvents[action.payload.date];
+      d.splice(d.indexOf(action.payload.start), 1);
+    },
   },
 });
 
-export const { setLabel } = slice.actions;
+export const { setLabel, addEvent, removeEvent } = slice.actions;
 
+export const selectEvents = (state) => state.infos.selectedEvents;
 export const selectLabel = (state) => state.infos.selectedLabel;
 
 export default slice.reducer;
