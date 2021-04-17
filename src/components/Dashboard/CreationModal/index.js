15915680cff3dd1af6a4f286cdf04988fcc6194b
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-04-17 02:07:23 +0200

    Changed the create event logic.
    When submitting, the user got shown a day when he submit this day another show until all days passed.

--- src/components/Dashboard/CreationModal/index.js
+++ src/components/Dashboard/CreationModal/index.js
@@ -7,13 +7,15 @@ import { hide, selectInfos } from '../../../features/modals/createSlice';
 import { fetchSubjects, selectSubjects } from '../../../features/database/subjectsSlice';
 
 import Selector from '../../Selector';
-import { selectEvents, editEvent } from '../../../features/infos/infosSlice';
+import { selectEvents, editEvent, selectLabel } from '../../../features/infos/infosSlice';
 
 import { getWeekDay, getMonth } from '../../../utils/Calendar';
 import Time from '../../../utils/Time';
+import { createEvent } from '../../../features/database/eventsSlice';
 
 // TODO : [ ] Focus on the field when visible.
 // TODO : [ ] Handle form submit.
+// TODO : [ ] Reset selected on when visible.
 
 const CreationModal = () => {
   const dispatch = useDispatch();
@@ -21,10 +23,12 @@ const CreationModal = () => {
   useEffect(() => dispatch(fetchSubjects()), []);
 
   const [selected, setSelected] = useState('');
+  const [selectedDay, setSelectedDay] = useState(0);
 
   const infos = useSelector(selectInfos);
   const subjects = useSelector(selectSubjects);
   const selectedEvents = useSelector(selectEvents);
+  const label = useSelector(selectLabel);
 
   const handleContentClick = (event) => event.stopPropagation();
   const handleClose = () => dispatch(hide());
@@ -35,6 +39,36 @@ const CreationModal = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    if (!selected) return;
+    if (!label) return;
+
+    const day = Object.keys(selectedEvents)[selectedDay];
+    for (const event of selectedEvents[day]) {
+      const descriptionEl = document.querySelector(`#start${event.start.replace(':', '_')}`);
+      const description = descriptionEl.value;
+
+      const [hour, min] = event.start.split(':').map(parseFloat);
+      const date = new Date(new Date(day).setHours(hour, min));
+
+      const obligatory = event.obligatory;
+
+      dispatch(
+        createEvent({
+          start: date,
+          description: description,
+          obligatory: obligatory,
+          label_name: label,
+          subject_name: selected,
+        })
+      );
+    }
+
+    // TODO : [ ] Stop if their is no others days after and show "Validation" instead of "Suivant". Check if the day isn't empty.
+    // TODO : [ ] Refetch all the events after creating some.
+
+    // setSelectedDay((prev) => prev + 1);
+    // handleSubmit(event);
   };
 
   const mapHours = (hours, day) => {
@@ -57,18 +91,38 @@ const CreationModal = () => {
 
       const getHourElement = (h1, h2) => {
         return (
-          <p className='hour' key={start}>
-            {h1} - {h2}
-            <span className={`pin ${obligatory ? 'red' : 'green'}`} onClick={handleClick}>
-              {obligatory ? 'Obligatoire' : 'Facultatif'}
-            </span>
-          </p>
+          <div className='hour' key={start}>
+            <p className='start'>
+              {h1} - {h2}
+              <span className={`pin ${obligatory ? 'red' : 'green'}`} onClick={handleClick}>
+                {obligatory ? 'Obligatoire' : 'Facultatif'}
+              </span>
+            </p>
+            <label className='description' htmlFor={`start${start.replace(':', '_')}`}>
+              Description
+              <input type='text' id={`start${start.replace(':', '_')}`} placeholder='Veuillez entrer une description.' />
+            </label>
+          </div>
         );
       };
 
       if (!prevHour && nextHour) return getHourElement(start, new Time(nextHour, 0).toString);
       if (!prevHour && !nextHour) return getHourElement(start, new Time(currHour + 1, 0).toString);
     });
+  };
+
+  const getFirstDay = () => {
+    const day = Object.keys(selectedEvents)[0];
+
+    if (selectedEvents[day] && selectedEvents[day].length > 0)
+      return (
+        <div className='day-container'>
+          <h3 className='day-name'>
+            {getWeekDay(new Date(day))} {new Date(day).getDate()} {getMonth(new Date(day))}
+          </h3>
+          <div className='hours-list'>{mapHours(selectedEvents[day], day)}</div>
+        </div>
+      );
   };
 
   return (
@@ -82,18 +136,7 @@ const CreationModal = () => {
         <div className='content'>
           <p className='description'>{infos.description}</p>
 
-          <div className='days-list'>
-            {Object.keys(selectedEvents).map((day) => {
-              return (
-                <div className='item' key={day}>
-                  <h3 className='day-name'>
-                    {getWeekDay(new Date(day))} {new Date(day).getDate()} {getMonth(new Date(day))}
-                  </h3>
-                  <div className='hours-list'>{mapHours(selectedEvents[day], day)}</div>
-                </div>
-              );
-            })}
-          </div>
+          {getFirstDay()}
 
           <div className='fields'>
             <div className='form-group'>
@@ -103,10 +146,11 @@ const CreationModal = () => {
                 setSelected={handleSetSelected}
                 placeholder='Choisir un sujet'
                 className='field'
+                onSubmit={handleSubmit}
                 noValidation
               />
               <button type='submit' className={`submit-button ${!selected && 'disabled'}`}>
-                Valider
+                Suivant
               </button>
             </div>
           </div>
