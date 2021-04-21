d328a6980acb44d42910fb6a11cc27d889f08427
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-04-21 19:53:17 +0200

    Changed the endpoint of the database.

--- src/app/database.js
+++ src/app/database.js
@@ -1,6 +1,6 @@
 import { GraphQLClient } from 'graphql-request';
 
-const endpoint = 'http://localhost:5000/graphql';
+const endpoint = 'http://192.168.0.24:5000/graphql';
 const client = new GraphQLClient(endpoint);
 
 export default client;
