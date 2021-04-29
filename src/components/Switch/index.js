7fd4972896cc5d988c5c36c66c2b72654b32937c
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-04-29 21:33:59 +0200

    Added a disabled prop to the switch.

--- src/components/Switch/index.js
+++ src/components/Switch/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Switch = ({ choices, choice, setChoice }) => {
+const Switch = ({ choices, choice, setChoice, disabled = -1 }) => {
   if (Object.keys(choices).length !== 3) throw new Error('Switch must have 3 choices.');
 
   const handleClick = (choiceKey) => {
@@ -14,9 +14,13 @@ const Switch = ({ choices, choice, setChoice }) => {
 
   return (
     <div className='relative flex flex-row w-96 bg-gray-600 text-white rounded'>
-      {Object.keys(choices).map((choiceKey) => (
-        <div className='flex items-center justify-center w-1/3 cursor-pointer z-20' key={choiceKey} onClick={() => handleClick(choiceKey)}>
-          <p className='text-sm font-normal'>{choices[choiceKey]}</p>
+      {Object.keys(choices).map((choiceKey, index) => (
+        <div
+          className={`flex items-center justify-center w-1/3 z-20 ${index !== disabled ? 'cursor-pointer ' : 'cursor-not-allowed'}`}
+          key={choiceKey}
+          onClick={() => index !== disabled && handleClick(choiceKey)}
+        >
+          <p className={`text-sm font-normal select-none ${index !== disabled ? 'text-white' : 'text-gray-400'}`}>{choices[choiceKey]}</p>
         </div>
       ))}
 
@@ -29,6 +33,7 @@ Switch.propTypes = {
   choices: PropTypes.object,
   choice: PropTypes.any,
   setChoice: PropTypes.func,
+  disabled: PropTypes.number,
 };
 
 export default Switch;
