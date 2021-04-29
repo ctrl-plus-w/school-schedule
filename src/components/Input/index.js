c9c64a608616dc1245b520e48f329419ab4960e7
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-04-29 13:03:54 +0200

    Modified the Input component.

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -3,18 +3,18 @@ import PropTypes from 'prop-types';
 
 import { v4 } from 'uuid';
 
-const Input = ({ value, onChange, placeholder, icon, onClick }) => {
+const Input = ({ value, onChange, placeholder, icon, onClick, className, type }) => {
   const [id] = useState(v4());
 
   return (
-    <label className='form-control w-auto' htmlFor={id}>
+    <label className={`form-control w-auto ${className ? className : ''}`} htmlFor={id}>
       {icon && (
         <button type='button' className='input-icon' onClick={onClick} tabIndex='-1'>
           {icon}
         </button>
       )}
 
-      <input type='text' className='input' id={id} placeholder={placeholder} value={value} onChange={onChange} />
+      <input type={type} className='input' id={id} placeholder={placeholder} value={value} onChange={onChange} />
     </label>
   );
 };
@@ -25,6 +25,8 @@ Input.propTypes = {
   placeholder: PropTypes.string,
   icon: PropTypes.any,
   onClick: PropTypes.func,
+  className: PropTypes.any,
+  type: PropTypes.string,
 };
 
 export default Input;
