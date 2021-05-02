12a7a7b7f9ee044123168c1c2f7afea433a0a215
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-05-02 16:31:00 +0200

    Removed the loader from the dashboards.

--- src/components/Dashboard/Professor/index.js
+++ src/components/Dashboard/Professor/index.js
@@ -1,25 +1,21 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import Topbar from './Topbar';
 import Grid from './Grid';
 import EditModal from '../EditModal';
 import PlanModal from '../PlanModal';
-import Loader from '../../Loader';
 import Tooltip from '../../Tooltip';
 
-import { isLoading, fetchOwnedEvents } from '../../../features/database/eventsSlice';
+import { fetchOwnedEvents } from '../../../features/database/eventsSlice';
 
 const ProfessorDashboard = () => {
   const dispatch = useDispatch();
 
   useEffect(() => dispatch(fetchOwnedEvents()), []);
 
-  const loading = useSelector(isLoading);
-
   return (
     <div className='flex flex-col w-screen h-screen p-12 box-border'>
-      <Loader loading={loading && false} />
       <PlanModal />
       <EditModal />
       <Tooltip />
--- src/components/Dashboard/Student/index.js
+++ src/components/Dashboard/Student/index.js
@@ -1,25 +1,21 @@
 import React, { useEffect } from 'react';
 
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import Topbar from './Topbar';
 import Grid from './Grid';
 import EditModal from '../EditModal';
-import Loader from '../../Loader';
 import Tooltip from '../../Tooltip';
 
-import { fetchEvents, isLoading } from '../../../features/database/eventsSlice';
+import { fetchEvents } from '../../../features/database/eventsSlice';
 
 const StudentDashboard = () => {
   const dispatch = useDispatch();
 
   useEffect(() => dispatch(fetchEvents()), []);
 
-  const loading = useSelector(isLoading);
-
   return (
     <div className='flex flex-col w-screen h-screen p-12 box-border'>
-      <Loader loading={loading} />
       <EditModal />
       <Tooltip />
 
