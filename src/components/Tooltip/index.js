67fa0738140838ce7f0cddb2f9f659891ac3336d
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-05-02 17:53:24 +0200

    Handled the placement depending on the client x and y.

--- src/components/Tooltip/index.js
+++ src/components/Tooltip/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { createRef, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import { selectInfos } from '../../features/modals/tooltipSlice';
@@ -9,34 +9,43 @@ const Tooltip = () => {
   const [y, setY] = useState(-1000);
 
   const infos = useSelector(selectInfos);
+  const element = createRef();
 
   useEffect(() => {
     const listener = ({ clientX, clientY }) => {
-      if (!clientX || !clientY) return;
+      if (!clientX || !clientY || !element.current) return;
 
-      setX(clientX);
-      setY(clientY);
+      const { width, height } = element.current.getBoundingClientRect();
+      const { innerHeight, innerWidth } = window;
+
+      const maxWidth = innerWidth - width * 2;
+      const maxHeight = innerHeight - height * 2;
+
+      setX(clientX > maxWidth ? clientX - width : clientX);
+      setY(clientY > maxHeight ? clientY - height : clientY);
     };
 
     if (infos.visible) window.addEventListener('mousemove', listener);
     return () => window.removeEventListener('mousemove', listener);
-  }, [infos.visible]);
+  }, [infos.visible, element]);
 
   const style = { left: `${x}px`, top: `${y}px`, opacity: infos.visible ? '100%' : '0%' };
 
   return (
-    <div className='absolute flex flex-col p-4 bg-black text-white pointer-events-none transition-opacity z-50' style={style}>
-      <div className='flex flex-col'>
-        <div className='flex items-center mb-0.5'>
-          <p className='text-lg font-bold'>{infos.title}</p>
-          <Badge content={infos.obligatory ? 'Obligatoire' : 'Facultatif'} className='ml-2' />
+    <div className='fixed top-0 left-0 w-screen h-screen pointer-events-none z-50'>
+      <div ref={element} className='absolute flex flex-col p-4 bg-black text-white pointer-events-none transition-opacity' style={style}>
+        <div className='flex flex-col'>
+          <div className='flex items-center mb-0.5'>
+            <p className='text-lg font-bold'>{infos.title}</p>
+            <Badge content={infos.obligatory ? 'Obligatoire' : 'Facultatif'} className='ml-2' />
+          </div>
+          <p className='text-base font-normal'>{infos.description}</p>
         </div>
-        <p className='text-base font-normal'>{infos.description}</p>
-      </div>
 
-      <div className='flex justify-between mt-4'>
-        <p className='text-base font-bold mr-8'>{infos.fieldName}</p>
-        <p className='text-base font-normal'>{infos.fieldContent}</p>
+        <div className='flex justify-between mt-4'>
+          <p className='text-base font-bold mr-8'>{infos.fieldName}</p>
+          <p className='text-base font-normal'>{infos.fieldContent}</p>
+        </div>
       </div>
     </div>
   );
