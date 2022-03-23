0792631e19f675a7f16b11b0c1045307c0f0f99c
ctrl-plus-w <lukas.ldrn@gmail.com>
2022-03-23 20:49:32 +0100

    Updated the default server url

--- src/app/database.js
+++ src/app/database.js
@@ -1,6 +1,6 @@
 import { GraphQLClient } from 'graphql-request';
 
-const endpoint = 'http://localhost:500/grapqhl';
+const endpoint = 'http://localhost:5000/graphql';
 const client = new GraphQLClient(endpoint);
 
 export default client;
